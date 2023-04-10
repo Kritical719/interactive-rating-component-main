bcbb42fab2379450ab1f3380e06c7fe580979f2f
Kritical719 <javier.araya23@gmail.com>
2023-04-10 12:24:54 -0400

    Terminado 10/04/2023

--- java.js
+++ java.js
@@ -7,4 +7,17 @@ function select(colorChange) {
 }
 
 
+function show(resultado){
+    if(resultado === document.getElementsByClassName('afterClick')){
+       document.getElementById('result').innerHTML = "5 out of 5";
+    }else{
 
+   
+    document.getElementById('result').innerHTML = "5 out of 5";
+}
+
+}
+
+/* HELP ME FIX IT PLEASE GOT STUCK PLEASE WILL POST IT AND HOPEFULLY ONE OF 
+YOU MIGHT BE ABLE TO GIVE ME A HAND ON HOW TO FIX IT OR IMPROVE I NEVER GOT IT TO SHOW
+THE RATING FROM ONE INDEX TO THE OTHER FEELSBADMAN*/
