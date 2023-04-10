bf2c9fff589746c28a6e84de13298043efb2a49f
Kritical719 <javier.araya23@gmail.com>
2023-04-10 02:34:43 -0400

    attempt to fix index 1 to index 2

--- java.js
+++ java.js
@@ -1,4 +1,8 @@
-function select(colorChange){
+function select(colorChange) {
+    const existingElement = document.querySelector('.afterClick');
+    if (existingElement && existingElement !== colorChange) {
+        existingElement.classList.remove('afterClick');
+    }
     colorChange.classList.toggle('afterClick');
 }
 
